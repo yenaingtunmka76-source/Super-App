22267b426a1191819447df5e82b371f240ad9b0a
yenaingtunmka76-source <yenaingtunmka76@gmail.com>
2026-07-17 12:45:56 +0630

    Add installable PWA with Myitsone app icon

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE="myitkyina-guide-v1";
+const CORE=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET"||new URL(event.request.url).origin!==self.location.origin)return;
+  if(event.request.mode==="navigate"){
+    event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put("./index.html",copy));return response}).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response})));
+});
